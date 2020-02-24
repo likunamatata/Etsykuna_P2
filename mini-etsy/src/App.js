02189dda5f8c51ecdd6211edc5afb8c75211746b
Lia Kapanadze <liakapanadze@Lias-MacBook.local>
2020-02-24 16:48:56 -0500

    created React app

---
+++ mini-etsy/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mini Etsy will be built here</h1>
+    </div>
+  );
+}
+
+export default App;
